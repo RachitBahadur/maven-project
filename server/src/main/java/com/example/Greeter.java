e375eccbb220429388223700be958ca5267ccf7f
root <root@ip-172-31-17-245.ec2.internal>
2019-03-22 02:25:49 +0000

    changed again

--- server/src/main/java/com/example/Greeter.java
+++ server/src/main/java/com/example/Greeter.java
@@ -12,8 +12,10 @@ public class Greeter {
 
   }
  
-  /** comment is required here.
- */
+  /** 
+  * @param someone the name if a peron
+  * @return greeting string
+  */
   public final String greet(final String someone) {
     return String.format("Hello, %s!", someone);
   }
